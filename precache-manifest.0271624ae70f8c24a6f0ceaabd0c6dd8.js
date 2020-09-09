2bef1071f252b940d0d62d3654a2729f2c82b42f
Nikhil banuri <nikhilbanuri4@gmail.com>
2020-09-09 17:12:27 +0530

    First commit

---
+++ precache-manifest.0271624ae70f8c24a6f0ceaabd0c6dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b9b49c74f440659ba9349ae37a2996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ff83f25bbc9eb8cfaa4",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9371d24eb7ae8e0dcff8",
+    "url": "/static/js/2.444586cd.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.444586cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff83f25bbc9eb8cfaa4",
+    "url": "/static/js/main.38bbfdc3.chunk.js"
+  },
+  {
+    "revision": "675a8bac7a6b5f4eede5",
+    "url": "/static/js/runtime-main.a7a54b79.js"
+  }
+]);
